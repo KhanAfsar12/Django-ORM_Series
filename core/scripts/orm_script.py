9f5d2da992eac146b9aa422613a0e1c76a083c16
KhanAfsar12 <ka484564@gmail.com>
2025-04-03 18:46:31 +0530

    Q Object

--- core/scripts/orm_script.py
+++ core/scripts/orm_script.py
@@ -1,19 +1,16 @@
 import random
 from core.models import Rating, Restaurant, Sale, Staff, StaffRestaurant
-from django.db.models.functions import Upper
-from django.db.models import Count, Avg, Min, Max, Sum
+from django.db.models.functions import Upper, Length, Concat
+from django.db.models import Count, Avg, Min, Max, Sum, CharField, Value, Q
 from django.utils import timezone
 from django.db import connection
 from django.contrib.auth.models import User
 from pprint import pprint
 def run():
-    one_month_ago = timezone.now() - timezone.timedelta(days=31)
+   
+   
+    name_has_num = Q(restaurant__name__regex = r"[0-9]+")
+    print(Sale.objects.filter(name_has_num).values_list('restaurant__name', flat=True))
 
-    sales = Sale.objects.filter(datetime__gte = one_month_ago)
-    print(sales.aggregate(
-        min = Min('income'),
-        max = Max('income'),
-        avg = Avg('income'),
-        sum = Sum('income')
-        ))
-    # print(connection.queries)
+
+    pprint(connection.queries)
